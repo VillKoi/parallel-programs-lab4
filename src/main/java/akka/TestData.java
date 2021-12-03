81baa273dbb26cf188bbe25286d838f85866e365
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-04 02:35:33 +0300

    Scripted auto-commit on change (2021-12-04 02:35:33) by gitwatch.sh

--- src/main/java/akka/TestData.java
+++ src/main/java/akka/TestData.java
@@ -1,16 +1,21 @@
 package akka;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
 public class TestData {
+    private String packageID;
+
     public ArrayList<Object> GetTests() {
         return new ArrayList<>();
     }
 
     @JsonCreator
-    public ParceData {
-
+    public TestData(
+            @JsonProperty("packageID") String packageID
+    ) {
+      this.packageID = packageID;
     }
 }
