116122f92121044da767ff87f26d93936ea4a8b7
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-03 21:56:48 +0300

    Scripted auto-commit on change (2021-12-03 21:56:48) by gitwatch.sh

--- src/main/java/akka/AkkaApp.java
+++ src/main/java/akka/AkkaApp.java
@@ -1,4 +1,19 @@
 package akka;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class AkkaApp {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: FlightApp <1: input path FlightMapper> <2: input path AirportMapper> <output path>");
+            System.exit(-1);
+        }
+
+
+        ActorSystem system = ActorSystem.create("test");
+        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class) );
+        storeActor.tell(new StoreActor.StoreMessage("test", "test"), ActorRef.noSender());
+    }
 }
