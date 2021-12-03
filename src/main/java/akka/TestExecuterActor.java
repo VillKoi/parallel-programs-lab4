ccf41ef42f0b022a81acbde34fa6097042c20892
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-04 00:27:57 +0300

    Scripted auto-commit on change (2021-12-04 00:27:57) by gitwatch.sh

--- src/main/java/akka/TestExecuterActor.java
+++ src/main/java/akka/TestExecuterActor.java
@@ -6,7 +6,7 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class TestExecuterActor {
-    public String RunTest(String jscript, String functionName, String params) throws ScriptException, NoSuchMethodException {
+    public String RunTest(String jscript, String functionName, Object params) throws ScriptException, NoSuchMethodException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval(jscript);
         Invocable invocable = (Invocable) engine;
