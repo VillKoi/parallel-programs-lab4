20b171f60e4fdb3ea575304234dc744dc007625b
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-03 22:16:22 +0300

    Scripted auto-commit on change (2021-12-03 22:16:22) by gitwatch.sh

--- src/main/java/akka/TestExecuterActor.java
+++ src/main/java/akka/TestExecuterActor.java
@@ -1,7 +1,14 @@
 package akka;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestExecuterActor {
-    public void RunTest() {
-        
+    public void RunTest(String functionName, ) {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
     }
 }
