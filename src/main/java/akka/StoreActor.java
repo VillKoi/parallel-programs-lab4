0024bf014e4172f785446cb5549ae398964bc738
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-03 22:14:05 +0300

    Scripted auto-commit on change (2021-12-03 22:14:05) by gitwatch.sh

--- src/main/java/akka/StoreActor.java
+++ src/main/java/akka/StoreActor.java
@@ -1,6 +1,11 @@
 package akka;
 
 public class StoreActor {
+    public void SetTestResult(String testResult, String packageID) {
+      // сохраняем в локальное хранилище
+    };
 
-
+    public String GetTestResults(String packageID) {
+        return "";
+    }
 }
