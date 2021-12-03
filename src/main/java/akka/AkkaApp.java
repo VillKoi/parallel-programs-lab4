06b725ecda7bbe00ca82e2e7bb99c7c3149194ce
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-04 01:56:26 +0300

    Scripted auto-commit on change (2021-12-04 01:56:26) by gitwatch.sh

--- src/main/java/akka/AkkaApp.java
+++ src/main/java/akka/AkkaApp.java
@@ -54,12 +54,18 @@ public class AkkaApp {
                                     completeOKWithFuture(res, Jackson.marshaller());
                                 })))
                 )),
-                get(() -> path("run", ()->
+                get(() -> concat(
+                        path("get_result", ()->
                                 get(()-> parameter("packageID", key -> {
                                     Future<Object> res = Patterns.ask(storeActor, "message", 0);
                                     completeOKWithFuture(res, Jackson.marshaller());
-                                })
-                )));
+                                }))),
+                        path("run", ()->
+                                get(()-> parameter("packageID", key -> {
+                                    Future<Object> res = Patterns.ask(storeActor, "message", 0);
+                                    completeOKWithFuture(res, Jackson.marshaller());
+                                })))
+                ));
         );
 
 
